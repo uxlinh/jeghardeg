c82a4312717f1a93a3094df4834ee3a46fed62d2
uxlinh <linh.tn94@gmail.com>
2020-10-20 19:54:40 +0200

    .

--- js/main.js
+++ js/main.js
@@ -8,3 +8,28 @@ dropdownBtn.addEventListener('click', (event) => {
 dropdownBtn.addEventListener('focusout', (event) => {
 	dropdownContentEml.classList.remove('active');
 });
+
+
+//HERO ANIMATION
+const hero = document.querySelector(".header__textbox");
+const arrowdown = document.querySelector(".btn-arrow");
+
+const tl = new TimelineMax();
+tl.fromTo(
+    hero,
+    3, {
+        opacity: '0'
+    }, {
+        opacity: '1',
+        ease: Power2.easeInOut
+    },
+).fromTo(
+    arrowdown,
+    3, {
+        opacity: '0',
+        y: '-50%'
+    }, {
+        opacity: '1',
+        y: '10%',
+        ease: Power2.easeInOut
+    }, '-=2.1');
